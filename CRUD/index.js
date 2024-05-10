b5f2a3c4acdf3083a8a95e5e5919d7dbba6c40e7
Tiago Moraes <123968220+TiagoTLM@users.noreply.github.com>
2024-05-10 17:28:15 -0300

    update

--- CRUD/index.js
+++ CRUD/index.js
@@ -1,24 +1,89 @@
-//Javascript para validação de usuário
+//Validando os campos de Email e Senha
+//Verifica se o campo Email está vazio e se é válido
+//Se o email for válido -> Botão Recuperar senha será habilitado
+function onChangeEmail(){
+    toggleButtonsDisable();
+    toggleEmailErrors();   
+}
+function onChangeSenha(){
+    toggleButtonsDisable();
+    togglePasswordErrors();
+}
 
-/*function validadeFields(){
-    const usuario = document.getElementById("nome_usuario").value;
-    if (!usuario){
-        document.getElementById('recover-password-button').disabled = true;
-    } else if{
+//Verificando validade do email
+function isEmailValid(){
+    const email = form.email().value;
 
+    if(!email){
+        return false;
     }
-}*/    // Isso fica em Hold
+    return validateEmail(email);
+}
 
 
-//validando Password
-//constante
-/*const password = isPasswordValid();
-document.getElementById('login').disabled = !isPasswordValid;
-
+//Validando a senha
 function isPasswordValid(){
-        const password = document.getElementById('senha').value;
-        if (!password){
-            return false;
-        }
-        return true;
-}*/  //Deixa para depois
+    const password = form.senha().value;
+
+    if (!password) {
+        return false;
+    }
+    return true;
+}
+
+function toggleEmailErrors(){   //Função que alterna entre os erros para o email de login
+    const email = form.email().value;
+
+    if(!email) {
+        //Email obrigatório!!!
+        form.emailRequired().style.display = "block";
+    } else {
+        form.emailRequired().style.display = "none";
+    }
+
+    if (validateEmail(email)){
+        form.emailInvalidError().style.display = "none";
+    } else {
+        form.emailInvalidError().style.display = "block";
+    }
+}
+
+function togglePasswordErrors(){
+    const password = form.senha().value;
+
+    if(!password) {
+        form.obrigSenha().style.display = "block";
+    } else {
+        form.obrigSenha().style.display = "none";
+    }
+}
+
+function toggleButtonsDisable(){
+    const emailValid = isEmailValid();
+    form.recoverBtn().disabled = !emailValid;
+    
+    const passwordValid = isPasswordValid();
+    form.loginBtn().disabled = !emailValid || !passwordValid;  //Botão desabilitado caso as duas funções sejam False
+}
+
+//Redirecionando para a tela de Cadastros
+function login() {  
+    window.location.href = "cadastro.html"
+    
+    // Exibindo informações de localização no console
+    console.log('### window', window);
+    console.log('### window location', window.location);
+} 
+
+//Encapsulando o código
+const form = {
+    email: () => document.getElementById('email'),
+    senha: () => document.getElementById('senha'),
+    emailInvalidError: () => document.getElementById('avisoLogin'),
+    emailRequired: () => document.getElementById('obrigLogin'),
+    loginBtn: () => document.getElementById('login-btn'),
+    recoverBtn: () => document.getElementById('recover-btn'),
+    obrigSenha: () => document.getElementById('obrigSenha')
+}
+
+
