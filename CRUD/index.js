c3794d4f9d2622934ffa36f60911dc73f342d6c5
Tiago Moraes <123968220+TiagoTLM@users.noreply.github.com>
2024-05-10 12:04:44 -0300

    CRUD

---
+++ CRUD/index.js
@@ -0,0 +1,24 @@
+//Javascript para validação de usuário
+
+/*function validadeFields(){
+    const usuario = document.getElementById("nome_usuario").value;
+    if (!usuario){
+        document.getElementById('recover-password-button').disabled = true;
+    } else if{
+
+    }
+}*/    // Isso fica em Hold
+
+
+//validando Password
+//constante
+/*const password = isPasswordValid();
+document.getElementById('login').disabled = !isPasswordValid;
+
+function isPasswordValid(){
+        const password = document.getElementById('senha').value;
+        if (!password){
+            return false;
+        }
+        return true;
+}*/  //Deixa para depois
