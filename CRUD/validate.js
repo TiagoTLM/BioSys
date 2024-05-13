0a2611a89a93a2eba9581959e3395da32de51123
Tiago Moraes <123968220+TiagoTLM@users.noreply.github.com>
2024-05-13 17:38:28 -0300

    Update validate.js

--- CRUD/validate.js
+++ CRUD/validate.js
@@ -1,3 +1,8 @@
 function validateEmail(email){
     return /\S+@\S+\.\S+/.test(email);
 }
+
+//Um teste para logar no registro de novos usuários
+function validateAdmin(emailADM){
+    return 'tiagotlm@icloud.com'(emailADM);
+}
