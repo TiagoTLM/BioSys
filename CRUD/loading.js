5f727eadef9f00f76b1d1be7aa1758fbdc840f28
Tiago Moraes <tiagotlm@live.com>
2024-06-12 10:38:27 -0300

    loadUPDT

--- CRUD/loading.js
+++ CRUD/loading.js
@@ -2,38 +2,38 @@
 
 // Função para mostrar o aviso de "carregando"
 function showLoading() {
-   // Verifica se já existe um elemento de carregamento
-   if (!document.querySelector('.loading')) {
-       // Cria um elemento <div>
-       const div = document.createElement("div");
-       // Adiciona a classe "loading" ao <div>
-       div.classList.add("loading");
-       div.setAttribute('aria-live', 'polite');
-       div.setAttribute('aria-busy', 'true');
+    // Verifica se já existe um elemento de carregamento
+    if (!document.querySelector('.loading')) {
+        // Cria um elemento <div>
+        const div = document.createElement("div");
+        // Adiciona a classe "loading" ao <div>
+        div.classList.add("loading");
+        div.setAttribute('aria-live', 'polite');
+        div.setAttribute('aria-busy', 'true');
 
-       // Cria um elemento <img>
-       const img = document.createElement("img");
-       // Define o caminho da imagem para o GIF de carregamento
-       img.src = "loading.gif";
-       img.alt = "Carregando..."; // Texto alternativo caso a imagem não carregue
+        // Cria um elemento <img>
+        const img = document.createElement("img");
+        // Define o caminho da imagem para o GIF de carregamento
+        img.src = "loading.gif";
+        img.alt = "Carregando..."; // Texto alternativo caso a imagem não carregue
 
-       // Adiciona a <img> como filha do <div>
-       div.appendChild(img);
+        // Adiciona a <img> como filha do <div>
+        div.appendChild(img);
 
-       // Adiciona o <div> ao corpo do documento
-       document.body.appendChild(div);
-   }
+        // Adiciona o <div> ao corpo do documento
+        document.body.appendChild(div);
+    }
 }
 
 // Função para ocultar o aviso de "carregando"
 function hideLoading() {
-   // Seleciona o elemento com a classe "loading"
-   const loading = document.querySelector('.loading');
-   // Se o elemento existir
-   if (loading) {
-       // Remove o elemento
-       loading.remove();
-   }
+    // Seleciona o elemento com a classe "loading"
+    const loading = document.querySelector('.loading');
+    // Se o elemento existir
+    if (loading) {
+        // Remove o elemento
+        loading.remove();
+    }
 }
 
 window.addEventListener('load', hideLoading); // Garante que o loading seja removido ao carregar a página
