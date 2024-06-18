4e916606f44fe31510e01050627e7a325166233d
Tiago Moraes <tiagotlm@live.com>
2024-06-18 16:21:42 -0300

    Cadastro

--- CRUD/javascript/validate.js
+++ CRUD/javascript/validate.js
@@ -6,3 +6,23 @@ function validateEmail(email){
 function validateAdmin(emailADM){
     return 'tiagotlm@icloud.com'(emailADM);
 }
+
+function validateIdade() {
+    var idadeInput = document.getElementById('idade');
+    var valor = idadeInput.value;
+
+    // Remover qualquer caractere que não seja dígito
+    idadeInput.value = valor.replace(/[^0-9]/g, '');
+    
+    // Limitar o comprimento da entrada a 3 caracteres
+    if (valor.length > 3) {
+        idadeInput.value = valor.slice(0, 3);
+    }
+
+    // Assegurar que o valor está dentro dos limites permitidos
+    if (idadeInput.value > 999) {
+        idadeInput.value = 999;
+    } else if (idadeInput.value < 0) {
+        idadeInput.value = 0;
+    }
+}
