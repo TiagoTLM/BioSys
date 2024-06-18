5a98abd18c7ca393642734ac4b5dd7a1a7eef271
Tiago Moraes <tiagotlm@live.com>
2024-06-18 11:28:06 -0300

    backButtonJS

---
+++ CRUD/javascript/backButton.js
@@ -0,0 +1,14 @@
+function backButton(){
+    document.addEventListener('DOMContentLoaded', function () {
+        const redirectButton = document.getElementById('backButton');
+    
+        if (redirectButton) {
+            redirectButton.addEventListener('click', function () {
+                showLoading(); // Mostra o indicador de loading
+                setTimeout(function () {
+                    window.location.href = '../html/home.html'; // Redireciona para home.html após um pequeno delay (opcional)
+                }, 1000); // Ajuste o tempo de delay conforme necessário (1000 milissegundos = 1 segundo)
+            });
+        }
+    });
+}
