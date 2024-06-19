bfc5a8c9c170e1ca5107567cbc7c7c17e58e5ffc
Tiago Moraes <123968220+TiagoTLM@users.noreply.github.com>
2024-06-19 17:11:04 -0300

    updt

--- CRUD/javascript/home.js
+++ CRUD/javascript/home.js
@@ -1,19 +1,18 @@
 // Função para logout
 function logout() {
-    console.log("Tentando fazer logout...");
     
     showLoading(); // Mostra o indicador de carregamento
-    
-    firebase.auth().signOut()
-        .then(() => {
-            console.log("Usuário deslogado com sucesso");
-            window.location.href = "../html/index.html"; // Redireciona para a página de login após logout
-        })
-        .catch((error) => {
-            console.error("Erro ao fazer logout:", error);
-            alert("Erro ao fazer logout. Por favor, tente novamente.");
-        })
-        .finally(() => {
-            hideLoading(); // Esconde o indicador de carregamento independentemente do resultado
-        });
+
+firebase.auth().signOut()
+    .then(() => {
+        console.log("Usuário deslogado com sucesso");
+        window.location.href = "../html/index.html"; // Redireciona para a página de login após logout
+    })
+    .catch((error) => {
+        console.error("Erro ao fazer logout:", error);
+        alert("Erro ao fazer logout. Por favor, tente novamente.");
+    })
+    .finally(() => {
+        hideLoading(); // Esconde o indicador de carregamento independentemente do resultado
+    });
 }
