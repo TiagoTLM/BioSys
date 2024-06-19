2b94644ec6911f132db99e5989e9e11e6b3e9587
Tiago Moraes <123968220+TiagoTLM@users.noreply.github.com>
2024-06-19 14:21:57 -0300

    review

--- CRUD/javascript/home.js
+++ CRUD/javascript/home.js
@@ -1,7 +1,7 @@
 //Deslogando o user
 function logout(){
     firebase.auth().signOut().then( () => {
-        window.location.href = '../html/home.html';
+        window.location.href = '../html/index.html';
     }).catch( () => {
         alert('Erro ao fazer Logout.');
     })
--- CRUD/javascript/loading.js
+++ CRUD/javascript/loading.js
@@ -1,36 +1,36 @@
 // Função para mostrar o aviso de "carregando"
 function showLoading() {
-        // Cria ndo um <div>
-        const div = document.createElement("div");
-       
-        // Adiciona a classe "loading" ao <div>
-        div.classList.add("loading");
-        
-        // Cria um elemento <img>
-        const img = document.createElement("img");
-        
-        // Define o caminho da imagem para o GIF de carregamento
-        img.src = "../image/loading.gif";
-        img.alt = "Carregando..."; // Texto alternativo caso a imagem não carregue
-        
-        // Adiciona a <img> como filha do <div>
-        div.appendChild(img);
+    // Cria ndo um <div>
+    const div = document.createElement("div");
+   
+    // Adiciona a classe "loading" ao <div>
+    div.classList.add("loading");
+    
+    // Cria um elemento <img>
+    const img = document.createElement("img");
+    
+    // Define o caminho da imagem para o GIF de carregamento
+    img.src = "../image/loading.gif";
+    img.alt = "Carregando..."; // Texto alternativo caso a imagem não carregue
+    
+    // Adiciona a <img> como filha do <div>
+    div.appendChild(img);
 
-        // Adiciona o <div> ao corpo do documento
-        document.body.appendChild(div);   
+    // Adiciona o <div> ao corpo do documento
+    document.body.appendChild(div);   
 }
 
 // Função para ocultar o aviso de "carregando"
 function hideLoading() {
-    // Seleciona o elemento com a classe "loading"
-    const loading = document.getElementsByClassName('loading');
-    // Se o elemento existir
-    if (loading.length) {
-        // Remove o elemento
-        loading[0].remove();
-    }
+// Seleciona o elemento com a classe "loading"
+const loading = document.getElementsByClassName('loading');
+// Se o elemento existir
+if (loading.length) {
+    // Remove o elemento
+    loading[0].remove();
 }
- 
+}
+
 
 /* Teste
 window.addEventListener('load', hideLoading); // Garante que o loading seja removido ao carregar a página
